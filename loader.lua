00065a1a80c2e8b58dd0c74cec5414455ec39a4e
adubwon <execomeback1@gmail.com>
2025-12-28 21:53:03 -0600

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -31,6 +31,7 @@ local GAME_SCRIPTS = {
     [3623096087] = "https://github.com/adubwon/geekUI/raw/refs/heads/main/Games/musclelegends.lua",
     [115499966198681] = "https://github.com/adubwon/geekUI/raw/refs/heads/main/Games/My%20Brainrot%20Army.lua",
     [3678761576] = "https://github.com/adubwon/geekUI/raw/refs/heads/main/Universal/warp.lua",
+    [77888146126370] = "https://github.com/adubwon/geekUI/raw/refs/heads/main/Universal/warp.lua",
 }
 
 local CURRENT_PLACE_ID = game.PlaceId
