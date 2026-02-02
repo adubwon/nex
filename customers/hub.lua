82d36d4a1cc7ee1d6d255cd48ed878c41fbd253c
adubwon <execomeback1@gmail.com>
2026-02-02 09:08:19 -0800

    Update hub.lua

--- customers/hub.lua
+++ customers/hub.lua
@@ -13,9 +13,9 @@ local CoreGui = game:GetService("CoreGui")
 --================================================================================--
 local Config = {
     -- Key System Settings
-    CORRECT_KEY = "abyssfr",
-    DISCORD_LINK = "https://discord.gg/abyssfr",
-    DISCORD_INVITE_CODE = "abyssfr",
+    CORRECT_KEY = "ezunban",
+    DISCORD_LINK = "https://discord.gg/ezunban",
+    DISCORD_INVITE_CODE = "ezunban",
     
     -- Key Storage
     KEY_STORAGE_FILE = "abysskey.json",
